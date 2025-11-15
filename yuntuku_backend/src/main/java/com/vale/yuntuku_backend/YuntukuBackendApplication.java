12bcadd8224ccf6cb475f422ed46597dd4a9e839
MAIvale <qianchen25@mails.ucas.ac.cn>
2025-11-15 17:37:16 +0800

    初始化项目

---
+++ yuntuku_backend/src/main/java/com/vale/yuntuku_backend/YuntukuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vale.yuntuku_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuntukuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuntukuBackendApplication.class, args);
+    }
+
+}
---
+++ yuntuku_backend/src/test/java/com/vale/yuntuku_backend/YuntukuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vale.yuntuku_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuntukuBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
